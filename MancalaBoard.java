fcb4147dbb0000751ab8538d9b2b37f6cba919c6
pheeellis <chiliphyllie@gmail.com>
2015-12-03 21:13:51 -0800

    Update MancalaBoard.java

--- MancalaBoard.java
+++ MancalaBoard.java
@@ -1,10 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -19,21 +17,25 @@ import javax.swing.event.ChangeListener;
 /*
  * defines a mancala board frame object
  */
-public class MancalaBoard extends JFrame implements ChangeListener {
+public class MancalaBoard implements ChangeListener {
 	
-	//numeber of pits for board game
+	//Number of pits for board game
 	private int ALL_PITS = 14;
 	private BoardStyle beez;
 	//number of pits per player
 	private int NUM_PITS = 6;
 	private Model m;
 	public int[] testArray = {0, 1, 2, 3, 4, 5, 6,   7, 8, 9, 10, 11, 12, 13};
+	public JFrame frame;
 	public JPanel pits;
 	public JPanel topPanel;		
 	public JPanel gBoard ;
+	public JPanel undoPanel;
 	
-	/*
-	 * frame constructor
+	/* frame constructor for mancala board frame
+	 * 
+	 * @param b the boardstyle to implement for the mancala board game 
+	 * @param model the data object for the mancala board game 
 	 */
 	public MancalaBoard(BoardStyle b, Model model)
 	{	
@@ -41,42 +43,45 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		this.m = model;
 		this.beez = b; 
 		this.m.addChangeListener(this);
-		setSize(400,500);
-		setLayout(new BorderLayout());
-		setBackground(Color.lightGray);
+		
+		//frame for game board
+		frame = new JFrame();
+		frame.setSize(400,500);
+		frame.setLayout(new BorderLayout());
+		frame.setBackground(Color.lightGray);
 		makeFrame(b);
 	}
 	
+	
+	/* adds to the main frame of the mancala board game 
+	 * 
+	 * @param b the boardstyle of the mancala booard game 
+	 */
 	public void makeFrame(BoardStyle b)
 	{
-		//frame for game board
-		//JFrame frame = new JFrame();
-		
-				
 		//add labels for the pits to the frame for players A and B
 		//make the game board and add it to the frame
 		topPanel = new JPanel();
 		topPanel.setLayout(new BorderLayout());
 		topPanel.add(makePlayerLabel("<----- Player B"), BorderLayout.NORTH);
-		//topPanel.add(makeLabels("B"), BorderLayout.CENTER);
 		topPanel.add(makeGameBoard(b), BorderLayout.SOUTH);
-		add(topPanel, BorderLayout.NORTH);
+		frame.add(topPanel, BorderLayout.NORTH);
 		
-		JPanel undoPanel = new JPanel();
+		undoPanel = new JPanel();
 		undoPanel.setLayout(new BorderLayout());	
-		//undoPanel.add(makeLabels("A"), BorderLayout.NORTH);
 		undoPanel.add(makePlayerLabel("Player A ----->"), BorderLayout.CENTER);
 		undoPanel.add(makeUndoBar(), BorderLayout.SOUTH);
-		add(undoPanel, BorderLayout.CENTER);
+		frame.add(undoPanel, BorderLayout.CENTER);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pack();
-		setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
-	/*
-	 * make the game board panel 
-	 * @return gBoard JPanel a moncala game board with pits and moncala pits
+	/* makes the panel that is the mancala board game 
+	 * 
+	 * @param b the boardstyle of the mancala board 
+	 * @return gBoard JPanel a mancala game board with pits and mancala pits
 	 */
 	public JPanel makeGameBoard(BoardStyle b)
 	{
@@ -93,10 +98,10 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		return gBoard;
 	}
 	
-	/*
-	 * make labels panel
-	 * @player the player who the labels will belong too
-	 * @return labels JPanel the labels panel for the given player
+	/* make labels panel
+	 * 
+	 * @param player the player who the labels will belong too
+	 * @return labels JPanel the label for the given players pits 
 	 */
 	public JPanel makeLabels(String player)
 	{
@@ -114,12 +119,16 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		return labels;
 	}
 	
-	/*
-	 * make the player pits for the game board
-	 * @return pits JPanel for the game board
+	/* make the player pits for the gameBoard
+	 * 
+	 * @return pits JPanel a JPanel containing pits for both players
 	 */
 	public JPanel makePits(BoardStyle b)
 	{
+		int stones;
+		int in;
+		boolean p;
+		
 		pits = new JPanel();
 		pits.setLayout(new GridLayout(2, 6));
 		
@@ -127,49 +136,44 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		{
 			String id;
 			final JButton pit;
-			final Pit myP; 
 			final PitIcon pitter;
 			if (j < 6)
 			{
 				id = "B" + "" + String.valueOf(String.valueOf((11-j)%6+1));
-				myP = new Pit(b, id, 75, m.get(12-j).getStonesFromPit(), 12-j, false);
-				pitter = new PitIcon(myP);
+				stones = m.get(12-j).getStonesFromPit();
+				in = 12-j;
+				p = false;
+				
+				pitter = new PitIcon(stones, in, p, b, 75);
 				pit = new JButton(pitter);
 				
 				pit.setText(id);
 				pit.setHorizontalTextPosition(JButton.CENTER);
 				pit.setVerticalTextPosition(JButton.NORTH);
-				   
-				//pit.setName(String.valueOf(11-j));
 			}
 			else
 			{
 				id =  "A" + "" + String.valueOf((j-6) +1);
-				myP = new Pit(b, id, 75, m.get(j-6).getStonesFromPit(), j-6, true);
-				pitter = new PitIcon(myP);
+				stones = m.get(j-6).getStonesFromPit();
+				in = j-6;
+				p = true;
+				
+				pitter = new PitIcon(stones, in, p, b, 75);
 				pit = new JButton(pitter);
 				
 				pit.setText(id);
 				pit.setHorizontalTextPosition(JButton.CENTER);
 				pit.setVerticalTextPosition(JButton.NORTH);
-				
 			}
 			
 			pit.addActionListener(new ActionListener()	{
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					String pName = myP.getPitId();
-					int sInPit = myP.getStonesFromPit();
-					int indexOfP = myP.getIndexOfPit();
-					
-					System.out.println("pitName: " + pName + ", #stonesInPit: " + sInPit + ", pitsIndexInArray: " + indexOfP);
-					
-					//myP.stonesPickedUp();  // sets current num of stones to 0, simulates stones picked up fromm pit when clicked on 
+					int sInPit = pitter.getStonesFromPit();
+					int indexOfP = pitter.getIndexOfPit();
+					//System.out.println("#stonesInPit: " + sInPit + ", pitsIndexInArray: " + indexOfP);
+	
 					m.moveStones(indexOfP);
-					//m.setIndex(myP.getIndexOfPit(), myP.getStonesFromPit());
-					pitter.update();
-					
-					pit.setIcon(new PitIcon(myP));
 				}
 			});
 			pits.add(pit);
@@ -177,9 +181,11 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		return pits;
 	}
 	
-	/*
-	 * make mancala pit 
-	 * @return a mancala pit JPanel
+	/* makes a mancala pit for the given player  
+	 * 
+	 * @param b the boardstyle to implement the moncala pit by
+	 * @param p the player whom the mancala pit will belong too
+	 * @return mancalaPit a JPanel with a mancalaPit in it 
 	 */
 	public JLabel makeMancalaPit(BoardStyle b, Boolean p)
 	{
@@ -190,18 +196,13 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		else	{
 			mancalaPit = new JLabel(new MancalaPit(b, 170, m.get(13).stonesInPit));
 		}
-		
-		//
-		
-		//mancalaPit = new JLabel(new MPit(null, null, 0, 0, i, false));
-				//new MancalaPit(b , 170, 0));
 		return mancalaPit;
 	}
 	
-	/*
-	 * make player label
-	 * @param s the string that defines one of the players
-	 * @return label a JLabel of player name
+	/* make player label
+	 * 
+	 * @param s the string that defines player A or player B
+	 * @return label a JLabel with the given player name
 	 */
 	public JLabel makePlayerLabel(String s)
 	{
@@ -209,29 +210,51 @@ public class MancalaBoard extends JFrame implements ChangeListener {
 		return label;
 	}
 	
+	/* makes a JPanel that houses the undo button as states players turn and 
+	 * how many undo's are left for the current player 
+	 * 
+	 * @return undoBar a JPanel with an undo button and information about the current state of the game 
+	 */
 	public JPanel makeUndoBar()
 	{
 		JPanel undoBar = new JPanel();
 		undoBar.setLayout(new BorderLayout());
 		
-		JButton undo = new JButton("Undo Last Move");
-		undoBar.add(undo, BorderLayout.EAST);
-		undoBar.add(new JLabel("         player turn         "), BorderLayout.WEST);
-		undoBar.add(new JLabel("         # of undo's used/left         "), BorderLayout.CENTER);
+		Boolean cPlayer = m.whichPlayer();
+		String pFiller;
+		int undoC = m.getUndoCount();
+		String spacer = "          ";
 		
+		if(cPlayer)	{
+			pFiller = "  Player A's Turn" + spacer;
+		}
+		else	{
+			pFiller = "  Player B's Turn" + spacer;
+		}
+		
+		JButton undo = new JButton("Undo Last Move");
+		undo.addActionListener(new ActionListener()	{
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				m.undo();
+			}		});
+		
+		String undoLabel =  spacer + "# of undo's left: " + String.valueOf(3 - undoC);
+		
+		undoBar.add(undo, BorderLayout.EAST);
+		undoBar.add(new JLabel(pFiller), BorderLayout.WEST);
+		undoBar.add(new JLabel(undoLabel), BorderLayout.CENTER);
 		return undoBar;
 	}
 
+	/*
+	 *  {@inheritDoc}
+	 */
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		//setVisible(false);
 		m.getPits();
 		topPanel.setVisible(false);
-		//refreshPits(beez);
+		undoPanel.setVisible(false);
 		makeFrame(beez);
-		//revalidate();
-		//repaint();
-		//makePits(beez);
-		//pits.repaint();
 	}
 }
